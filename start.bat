44b98a62941e9335ca310958bd3c176e0a55c75d
Wallauerr <wallauer@protonmail.com>
2026-08-27 17:23:34 -0300

    chore: remove start.bat (uso passa a ser pelo .exe)

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM ============================================
-REM  Start the Automated Credit Analysis (GUI)
-REM ============================================
-chcp 65001 >nul
-title Análise de Crédito - Serasa
-python src\app.py
