fca931c06fad82049f615da0ab656cd49c2a3c0b
Wallauerr <wallauer@protonmail.com>
2026-08-27 16:31:33 -0300

    chore: script de inicializacao (start.bat)

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+REM ============================================
+REM  Start the Automated Credit Analysis
+REM ============================================
+chcp 65001 >nul
+title Credit Analysis - Serasa
+python main.py
